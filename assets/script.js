590822c797663e78c5003ff4cc26447c4d01772e
Tyler Clark <tylerc111@gmail.com>
2023-12-25 15:46:42 -0500

    fixed file pathing for JS in the HTML file.

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ var apiKey = 'b70804e39be9e864cf9755c6056c4418';
 var latitude = "";
 var longitude = "";
 
-fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`)
+fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${b70804e39be9e864cf9755c6056c4418}`)
   .then(response => response.json())
   .then(data => {
     // Handle the weather data here
